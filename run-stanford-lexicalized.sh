a12838543d31a1bb4bc690c866279517ba2dcb49
Yash Shah <yashshah00@outlook.com>
2025-01-06 15:16:44 +0000

    Added support for the Stanford PCFG (unlexicalized) and Stanford Factored (lexicalized) parsers.

---
+++ run-stanford-lexicalized.sh
@@ -0,0 +1,10 @@
+STANFORD_DIRECTORY='stanford-parser-full-2015-01-30'
+if [ ! -d "$STANFORD_DIRECTORY" ]; then
+  echo "$STANFORD_DIRECTORY does not exist. Downloading."
+  wget http://nlp.stanford.edu/software/stanford-parser-full-2015-01-29.zip
+  unzip stanford-parser-full-2015-01-29.zip
+fi
+
+echo 'Running stanford lexicalized parser on input file.'
+
+java -mx600m -cp "$STANFORD_DIRECTORY/*:" edu.stanford.nlp.parser.lexparser.LexicalizedParser -outputFormat "penn" edu/stanford/nlp/models/lexparser/englishFactored.ser.gz input.txt >> "stanford-lexicalized-output.txt"
---
+++ run-stanford-unlexicalized.sh
@@ -0,0 +1,10 @@
+STANFORD_DIRECTORY='stanford-parser-full-2015-01-30'
+if [ ! -d "$STANFORD_DIRECTORY" ]; then
+  echo "$STANFORD_DIRECTORY does not exist. Downloading."
+  wget http://nlp.stanford.edu/software/stanford-parser-full-2015-01-29.zip
+  unzip stanford-parser-full-2015-01-29.zip
+fi
+
+echo 'Running stanford unlexicalized parser on input file.'
+
+java -mx150m -cp "$STANFORD_DIRECTORY/*:" edu.stanford.nlp.parser.lexparser.LexicalizedParser -outputFormat "penn" edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz input.txt >> "stanford-unlexicalized-output.txt"
