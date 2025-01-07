864f6cc84ae440fb6bc0f53ffd361c965976dd8b
Yash Shah <yashshah00@outlook.com>
2025-01-07 19:38:44 +0000

    Modified gold-flat.txt so it has a ROOT and added run-berkeley-parser-analyser.sh.

---
+++ run-berkeley-parser-analyser.sh
@@ -0,0 +1,8 @@
+if [ ! -f "stanford-unlexicalized-output.txt" ] || [ ! -f "stanford-lexicalized-output.txt" ] || [ ! -f "bubs-output.txt" ]; then 
+  echo "Run all the parsers first to generate output.";
+fi
+
+python3 berkeley-parser-analyser/berkeley_parse_analyser/classify_english.py gold-flat.txt stanford-unlexicalized-output.txt berkeley-parser-analyser-outputs/stanford-unlexicalized
+python3 berkeley-parser-analyser/berkeley_parse_analyser/classify_english.py gold-flat.txt stanford-lexicalized-output.txt berkeley-parser-analyser-outputs/stanford-lexicalized
+python3 berkeley-parser-analyser/berkeley_parse_analyser/classify_english.py gold-flat.txt bubs-output.txt berkeley-parser-analyser-outputs/bubs
+
