739ea70133088214e022aaf83a6b7f61a589fa7b
Yash Shah <yashshah00@outlook.com>
2025-01-07 15:37:42 +0000

    BUBS now utilizes the Split-Merge 6 (SM6) latent-variable grammar from the Berkeley Parser.

--- run-bubs.sh
+++ run-bubs.sh
@@ -1,15 +1,13 @@
 BUBS_DIRECTORY='bubs-parser'
 if [ ! -d "$BUBS_DIRECTORY" ]; then
   echo "$BUBS_DIRECTORY does not exist. Downloading."
-  wget https://www.dropbox.com/s/bppl5o7jwqsu8yt/bubs-parser-20150206.tgz?dl=0
-  mv 'bubs-parser-20150206.tgz?dl=0' bubs-parser-20150206.tgz
-  tar zxvf bubs-parser-20150206.tgz
+  wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/bubs-parser/bubs-parser-20131221.tgz
+  tar zxvf bubs-parser-20131221.tgz
   chmod a+x bubs-parser/parse
-  unzip stanford-corenlp-4.5.8.zip
 fi
 
 cd "$BUBS_DIRECTORY"
 
 echo 'Running BUBS parser on input file.'
 
-./parse -g wsj_l0mm_16_.55_6.gr.gz -fom wsj_l0mm_16_.55_6.lexfom.gz -ccClassifier wsj_cc.mdl.995 '../input.txt' | tail -n +3 | head -n -1 > '../bubs-output.txt'
+./parse -g eng.sm6.gr.gz -fom eng.sm6.fom.gz -beamModel eng.sm6.bcm.gz '../input.txt' | tail -n +3 | head -n -1 > '../bubs-output.txt'
