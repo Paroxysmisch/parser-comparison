d7e7c972d22bde5ec148ebb9e107a0b460d872da
Yash Shah <yashshah00@outlook.com>
2025-01-15 22:04:40 +0000

    Conducting analysis with 3 additional crowd-sourced sentences.

--- run-berkeley-parser-analyser.sh
+++ run-berkeley-parser-analyser.sh
@@ -2,7 +2,7 @@ if [ ! -f "stanford-unlexicalized-output.txt" ] || [ ! -f "stanford-lexicalized-
   echo "Run all the parsers first to generate output.";
 fi
 
-python3 berkeley-parser-analyser/berkeley_parse_analyser/classify_english.py gold-flat.txt stanford-unlexicalized-output.txt berkeley-parser-analyser-outputs/stanford-unlexicalized
-python3 berkeley-parser-analyser/berkeley_parse_analyser/classify_english.py gold-flat.txt stanford-lexicalized-output.txt berkeley-parser-analyser-outputs/stanford-lexicalized
-python3 berkeley-parser-analyser/berkeley_parse_analyser/classify_english.py gold-flat.txt bubs-output.txt berkeley-parser-analyser-outputs/bubs
+python3 berkeley-parser-analyser/berkeley_parse_analyser/classify_english.py gold-fixed.txt stanford-unlexicalized-output.txt berkeley-parser-analyser-outputs/stanford-unlexicalized
+python3 berkeley-parser-analyser/berkeley_parse_analyser/classify_english.py gold-fixed.txt stanford-lexicalized-output.txt berkeley-parser-analyser-outputs/stanford-lexicalized
+python3 berkeley-parser-analyser/berkeley_parse_analyser/classify_english.py gold-fixed.txt bubs-output.txt berkeley-parser-analyser-outputs/bubs
 
